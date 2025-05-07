f5958c24d4511063174b3474733ff5017b46a676
ppires2327 <piresneves27@gmail.com>
2025-05-07 12:54:27 +0100

    add loop

--- servidor.py
+++ servidor.py
@@ -6,19 +6,36 @@ ARQUIVO_SAIDA = 'recebido.pdf'
 
 # AF_INET stands for Address Family: Internet. Serve para usar ipv4
 # SOCK_STREAM tells the socket to use TCP (Transmission Control Protocol).
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as servidor:
-    servidor.bind((HOST, PORT))
-    servidor.listen()
-    print(f"Servidor aguardando conexões em {HOST}:{PORT}...")
 
-    conexao, endereco = servidor.accept()
-    with conexao:
-        print(f"Conectado por {endereco}")
-        with open(ARQUIVO_SAIDA, 'wb') as f:
-            while True:
-                dados = conexao.recv(4096)
-                if not dados:
-                    break
-                f.write(dados)
+while True:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as servidor:
+    
+        servidor.bind((HOST, PORT))
+    
+        servidor.listen()
+    
+        print(f"Servidor aguardando conexões em {HOST}:{PORT}...")
 
-        print(f"Arquivo salvo como {ARQUIVO_SAIDA}")
+
+    
+        conexao, endereco = servidor.accept()
+    
+        with conexao:
+    
+            print(f"Conectado por {endereco}")
+    
+            with open(ARQUIVO_SAIDA, 'wb') as f:
+    
+                while True:
+    
+                    dados = conexao.recv(4096)
+    
+                    if not dados:
+    
+                        break
+    
+                    f.write(dados)
+
+
+    
+            print(f"Arquivo salvo como {ARQUIVO_SAIDA}")
